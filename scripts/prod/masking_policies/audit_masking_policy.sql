8232b94bb307c831c982fafccb33cc3d313b8718
Rohit Sawane <rohit.r.sawane@kipi.bi>
2025-09-08 15:31:53 +0530

    tag_change

--- scripts/prod/masking_policies/audit_masking_policy.sql
+++ scripts/prod/masking_policies/audit_masking_policy.sql
@@ -1,5 +1,13 @@
 USE DATABASE {DATABASE_NAME};
 
+USE SCHEMA SCH_TRUSTCAB_AUDIT;
+
+create or replace tag PII_INFO  allowed_values  'NUMERIC_INFO' , 'STRING_INFO' COMMENT='Tag used for masking sensitive data'
+;
+
+create or replace tag SENSITIVITY COMMENT='Tag for classifying sensitive data (e.g., PII, FINANCIAL)'
+;
+
 create or replace masking policy SCH_TRUSTCAB_AUDIT.PII_MASK as (VAL NUMBER(38,0)) 
 returns NUMBER(38,0) ->
 CASE
--- scripts/prod/tags/audit_tags.sql
+++
@@ -1,8 +0,0 @@
-USE DATABASE {DATABASE_NAME};
-USE SCHEMA SCH_TRUSTCAB_AUDIT;
-
-create or replace tag PII_INFO  allowed_values  'NUMERIC_INFO' , 'STRING_INFO' COMMENT='Tag used for masking sensitive data'
-;
-
-create or replace tag SENSITIVITY COMMENT='Tag for classifying sensitive data (e.g., PII, FINANCIAL)'
-;
